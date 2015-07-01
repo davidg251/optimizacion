406c538b528d4afb050a8803e8d4deaa468f5db3
David Gomez <david.ernesto.gomez@correounivalle.edu.co>
2015-07-01 17:38:18 -0500

    inicial

---
+++ demo.c
@@ -0,0 +1,260 @@
+/*
+David Ernesto Gomez Gutierrez
+20/junio/2015
+Por hacer:
+-
+*/
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include "lp_lib.h"
+
+/*
+Variables globales
+*/
+int demandas_ofertas[6] = {0};
+int costos[9] = {600,700,500,320,300,350,500,480,510};
+int resultado[9] = {0};
+int funcion_objetivo = 0.0;
+FILE *archivo_entrada;
+FILE *archivo_salida;
+
+void leerArchivo()
+{
+  char ch;
+  char linea[100];
+  strcpy(linea,"");
+  char caracter[2];
+  int numero_temporal = 0;
+  int index = 0;
+
+  archivo_entrada = fopen("entrada.txt","r");
+
+  while( (ch = fgetc(archivo_entrada)) != EOF)
+  {
+
+    if(ch == '\n')
+    {
+      char *token;
+      token = strtok(linea, " ");
+
+      while (token != NULL)
+      {
+        numero_temporal = atoi(token);
+        demandas_ofertas[index] = numero_temporal;
+        token = strtok(NULL, " ");
+        index++;
+      }
+
+      strcpy(linea,"");
+    }
+    else
+    {
+      caracter[0] = ch;
+      caracter[1] = '\0';
+      strncat(linea,caracter,1);
+    }
+  }
+
+  fclose(archivo_entrada);
+
+}
+
+
+int ejecutarSimplex()
+{
+  lprec *lp;
+
+  REAL *row = NULL;
+  int *colno = NULL;
+  int i,j,k=0;
+  char str[5];
+  lp = make_lp(0,9);//crea modelo con 9 variables, 0 fil 9 col
+
+  if(lp == NULL) return 0;
+
+  for(i=1;i<10;i++)
+  {
+    sprintf(str,"x%i",i);
+
+    set_col_name(lp,i,str); //reservo espacio para variables del modelo
+  }
+
+  //reservo espacio para listas
+  colno = (int *) malloc(9 * sizeof(*colno));
+  row = (REAL *) malloc(9* sizeof(*row));
+
+  if((colno == NULL) || (row == NULL)) return 0;/*si no se crean*/
+
+  set_add_rowmode(lp, TRUE);/*hacer modelo fila*fila es "mas rapido"*/
+
+  /*
+	en este trozo de codigo se agregan todas las restricciones
+  */
+
+  for(j=0;j<3;j++)
+  {
+    i = 0;
+
+    colno[i] = j+1;
+    row[i++] = demandas_ofertas[0];
+
+    colno[i] = j+4;
+    row[i++] = demandas_ofertas[1];
+
+    colno[i] = j+7;
+    row[i++] = demandas_ofertas[2];
+
+    if(!add_constraintex(lp,i,row,colno,GE,demandas_ofertas[3+j])) return 0;
+
+    i = 0;
+    row[i++] = 1;
+    row[i++] = 1;
+    row[i++] = 1;
+
+    if(!add_constraintex(lp,i,row,colno,LE,1)) return 0;
+  }
+
+  i = 0;
+  colno[i] = 1;
+  row[i++] = 1;
+
+  colno[i] = 2;
+  row[i++] = 1;
+
+  colno[i] = 3;
+  row[i++] = 1;
+
+  if(!add_constraintex(lp,i,row,colno,LE,1)) return 0;
+
+
+  i = 0;
+  colno[i] = 4;
+  row[i++] = 1;
+
+  colno[i] = 5;
+  row[i++] = 1;
+
+  colno[i] = 6;
+  row[i++] = 1;
+
+  if(!add_constraintex(lp,i,row,colno,LE,1)) return 0;
+
+  i = 0;
+  colno[i] = 7;
+  row[i++] = 1;
+
+  colno[i] = 8;
+  row[i++] = 1;
+
+  colno[i] = 9;
+  row[i++] = 1;
+
+  if(!add_constraintex(lp,i,row,colno,LE,1)) return 0;
+
+
+  /*se agrega la funcion objetivo*/
+   set_add_rowmode(lp, FALSE);
+   k = -1;
+   i = 0;
+
+   for(j=1;j<10;j++)
+   {
+      k++;
+      colno[i] = j;
+	    row[i++] = costos[j-1] * demandas_ofertas[k+3] ;
+
+
+      if(k==2) k=-1;
+
+    }
+
+  if(!set_obj_fnex(lp, i, row, colno)) return 0;
+  set_minim(lp);
+
+  write_lp(lp, "model.lp");
+  //write_LP(lp, stdout);
+  //libero memoria
+
+  if(solve(lp) != OPTIMAL)
+  {
+    perror("Algo ha salido mal");
+  }
+  else
+  {
+    //printf("Valor funcion objetivo: %f\n", get_objective(lp));
+    funcion_objetivo = get_objective(lp);
+    get_variables(lp, row);
+
+    for(i=0;i<9;i++)
+    {
+      //printf("%s: %f \n",get_col_name(lp, i+1), row[i]);
+      resultado[i] = (int)round(row[i]);
+    }
+
+
+  }
+
+  delete_lp(lp);
+  free(row);
+  free(colno);
+
+}
+
+void escribirArchivo()
+{
+  int i,j=0;
+  char cadena_temporal[50];
+  archivo_salida = fopen("salida.txt","w");
+
+  for (i = 0;  i < 9; i++)
+  {
+    j++;
+    if(resultado[i])
+    {
+      switch (i)
+      {
+        case 0:
+        case 3:
+        case 6:
+        sprintf(cadena_temporal,"Planta %i: Palmira. \n",j);
+        fputs(cadena_temporal, archivo_salida);
+        break;
+
+        case 1:
+        case 4:
+        case 7:
+        sprintf(cadena_temporal,"Planta %i: Tulua. \n",j);
+        fputs(cadena_temporal, archivo_salida);
+        break;
+
+        case 2:
+        case 5:
+        case 8:
+        sprintf(cadena_temporal,"Planta %i: Cali. \n",j);
+        fputs(cadena_temporal, archivo_salida);
+        break;
+      }
+
+    }
+    funcion_objetivo += resultado[i] * costos[i];
+    if(j==3)j=0;
+  }
+
+  sprintf(cadena_temporal,"Funcion Objetivo: %i \n",funcion_objetivo);
+  fputs(cadena_temporal, archivo_salida);
+
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+
+  leerArchivo();
+  ejecutarSimplex();
+  escribirArchivo();
+  return 0;
+}
